46f02eaa58f21d6766bbfcc4d950c497460799e7
Shkelqim Bajraktari <shkelqim.bajraktari@edev-rks.com>
2024-01-15 10:06:44 +0100

    end of section 2

---
+++ API/Data/Migrations/20240112140414_InitialCreatee.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreatee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty");
+
+            migrationBuilder.RenameTable(
+                name: "MyProperty",
+                newName: "Products");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "MyProperty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty",
+                column: "Id");
+        }
+    }
+}
