c8e3505b6b55de3b904676d96824055db22804b9
Shkelqim Bajraktari <shkelqim.bajraktari@edev-rks.com>
2024-01-30 15:56:45 +0100

    end of section 7

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,30 @@
-import { Typography } from "@mui/material";
+import { ButtonGroup, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { decrement, increment } from "./counterSlice";
 
 export default function ContactPage() {
+    const dispatch = useAppDispatch(); 
+    const {data, title} = useAppSelector(state => state.counter);
+
+
+
     return(
-        <Typography variant="h2">
-            Contact Page
-        </Typography>
+        <>
+            <Typography variant="h2">
+                {title}
+            </Typography>
+            <Typography variant="h5">
+                The data is: {data}
+            </Typography>
+
+            <ButtonGroup>
+                <button onClick={() => dispatch(decrement(1))}  color='error'>Decrement</button>
+                <button onClick={() => dispatch(increment(1))}  color='primary'>Increment</button>
+                <button onClick={() => dispatch(increment(5))}  color='secondary'>Increment by 5</button>
+
+
+            </ButtonGroup>
+        </>
+
     )
 }
